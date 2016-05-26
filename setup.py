b080c33cb625509e673ffea012e0198342a9ba8f
Olav Vahtras <vahtras@kth.se>
2016-05-26 17:17:08 +0200

    More scripts, cleanup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="Ledger",
     version="0.1",
     packages=["ledges"],
-    scripts=['scripts/ledges'],
+    scripts=['scripts/ledges', 'scripts/generate_spectrum.sh'],
     author="Olav Vahtras",
     author_email="vahtras@kth.se"
     )
